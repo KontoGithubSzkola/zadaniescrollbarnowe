b088f5eaada523684e30f437aa8b102f9daf25a6
kontogithubszkola <julian.stajnke02@gmail.com>
2023-02-18 15:27:59 +0100

    2

--- app/src/main/java/com/example/zadaniescrollbar/MainActivity.kt
+++ app/src/main/java/com/example/zadaniescrollbar/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.zadaniescrollbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.SeekBar
 
@@ -9,24 +11,46 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    fun suma_poziomu(): Int{
+        val obraz1 = findViewById<ImageView>(R.id.imv)
+        val obraz2 = findViewById<ImageView>(R.id.imv2)
+        val obraz3 = findViewById<ImageView>(R.id.im4)
+
         val obraz1_poziom = findViewById<SeekBar>(R.id.poziom)
-        val obraz2_poziom = findViewById<SeekBar>(R.id.poziom2)
-        val obraz3_poziom = findViewById<SeekBar>(R.id.poziom3)
-        val suma = obraz1_poziom.progress + obraz2_poziom.progress + obraz3_poziom.progress
-        return suma
-    }
-
-    fun suma_pionu(): Int{
         val obraz1_pion = findViewById<SeekBar>(R.id.pion)
+        val obraz2_poziom = findViewById<SeekBar>(R.id.poziom2)
         val obraz2_pion = findViewById<SeekBar>(R.id.pion2)
+        val obraz3_poziom = findViewById<SeekBar>(R.id.poziom3)
         val obraz3_pion = findViewById<SeekBar>(R.id.pion3)
-        val suma = obraz1_pion.progress + obraz2_pion.progress + obraz3_pion.progress
-        return suma
-    }
+        val reset = findViewById<Button>(R.id.reset)
+        val progress_poziom = findViewById<ProgressBar>(R.id.pgbpoziom)
+        val progress_pion = findViewById<ProgressBar>(R.id.pgbpion)
 
-    fun changed(progressBar: ProgressBar, suma: Int){
-        progressBar.progress = suma/3
+        reset.setOnClickListener() {
+            obraz1_pion.progress = 100
+            obraz2_pion.progress = 100
+            obraz3_pion.progress = 100
+            obraz1_poziom.progress = 100
+            obraz2_poziom.progress = 100
+            obraz3_poziom.progress = 100
+        }
+        fun suma_poziomu(): Int {
+            val obraz1_poziom = findViewById<SeekBar>(R.id.poziom)
+            val obraz2_poziom = findViewById<SeekBar>(R.id.poziom2)
+            val obraz3_poziom = findViewById<SeekBar>(R.id.poziom3)
+            val suma = obraz1_poziom.progress + obraz2_poziom.progress + obraz3_poziom.progress
+            return suma
+        }
+
+        fun suma_pionu(): Int {
+            val obraz1_pion = findViewById<SeekBar>(R.id.pion)
+            val obraz2_pion = findViewById<SeekBar>(R.id.pion2)
+            val obraz3_pion = findViewById<SeekBar>(R.id.pion3)
+            val suma = obraz1_pion.progress + obraz2_pion.progress + obraz3_pion.progress
+            return suma
+        }
+
+        fun changed(progressBar: ProgressBar, suma: Int) {
+            progressBar.progress = suma / 3
+        }
     }
 }
